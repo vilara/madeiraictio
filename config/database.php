eeb491d999cf265b099486b00c6195e9fb188fcd
Marcelo Vilara <marcelovilara@gmail.com>
2020-06-05 11:26:00 -0300

    iniciolocaweb

--- config/database.php
+++ config/database.php
@@ -45,33 +45,14 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
-//         'mysql' => [
-//             'driver' => 'mysql',
-//             'host' => env('RDS_HOSTNAME', '127.0.0.1'),
-//             'port' => env('RDS_PORT', '3306'),
-//             'database' => env('RDS_DB_NAME', 'forge'),
-//             'username' => env('RDS_USERNAME', 'forge'),
-//             'password' => env('RDS_PASSWORD', ''),
-//             'unix_socket' => env('DB_SOCKET', ''),
-//             'charset' => 'utf8mb4',
-//             'collation' => 'utf8mb4_unicode_ci',
-//             'prefix' => '',
-//             'prefix_indexes' => true,
-//             'strict' => true,
-//             'engine' => null,
-//             'options' => extension_loaded('pdo_mysql') ? array_filter([
-//                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-//             ]) : [],
-//         ],
-
-        
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('RDS_HOSTNAME', '127.0.0.1'),
-            'port' => env('RDS_PORT', '3306'),
-            'database' => env('RDS_DB_NAME', 'forge'),
-            'username' => env('RDS_USERNAME', 'forge'),
-            'password' => env('RDS_PASSWORD', ''),
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'madeiraictio'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -79,8 +60,12 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ],
+        
+        
         
 
         'pgsql' => [
