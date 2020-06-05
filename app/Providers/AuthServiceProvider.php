40a9883c7bcaf6ab953880725a99e664d5883d0a
Marcelo Vilara <marcelovilara@gmail.com>
2020-06-05 10:05:58 -0300

    locawebInicio

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
@@ -25,6 +26,9 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        if(DB::connection()->getDatabaseName())
+        {
+            echo "Connected to database ".DB::connection()->getDatabaseName();
+        }
     }
 }
--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+
+
 return [
 
     /*
@@ -43,14 +45,33 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+//         'mysql' => [
+//             'driver' => 'mysql',
+//             'host' => env('RDS_HOSTNAME', '127.0.0.1'),
+//             'port' => env('RDS_PORT', '3306'),
+//             'database' => env('RDS_DB_NAME', 'forge'),
+//             'username' => env('RDS_USERNAME', 'forge'),
+//             'password' => env('RDS_PASSWORD', ''),
+//             'unix_socket' => env('DB_SOCKET', ''),
+//             'charset' => 'utf8mb4',
+//             'collation' => 'utf8mb4_unicode_ci',
+//             'prefix' => '',
+//             'prefix_indexes' => true,
+//             'strict' => true,
+//             'engine' => null,
+//             'options' => extension_loaded('pdo_mysql') ? array_filter([
+//                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+//             ]) : [],
+//         ],
+
+        
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('RDS_HOSTNAME', '127.0.0.1'),
+            'port' => env('RDS_PORT', '3306'),
+            'database' => env('RDS_DB_NAME', 'forge'),
+            'username' => env('RDS_USERNAME', 'forge'),
+            'password' => env('RDS_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -58,10 +79,9 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            
         ],
+        
 
         'pgsql' => [
             'driver' => 'pgsql',
